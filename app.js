27067ba69a7c359aa42cb2d6f2a5caeffdfe638e
Matias Vieira <matiasvieira0@gmail.com>
2021-04-20 00:09:00 -0300

    ejemplo CRUD products

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const app = express();
 
-// Rutas
+const productsRoutes = require('./routes/productsRoutes');
+const indexRoutes = require('./routes/indexRoutes');
+
+const methodOverride = require('method-override')   
+// Requerimos este módulo para asegurar compatibilidad de métodos PUT y DELETE en todos los navegadores.
+app.use(methodOverride('_method'))
+// Para configurarlo indicamos a app que use este método, con app.use lo empleamos a nivel aplicación.
+
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-app.get('/', (req, res) => res.send('Index'));
+// Rutas
+app.set('view engine', 'ejs');
 
-app.get('/products', (req, res) => {
-    res.send('Productos');
-});
+app.use('/', indexRoutes);
+app.use('/products', productsRoutes);
 
-app.get('/products/:id', (req, res) => {
-    res.send("Producto: " + req.params.id);
-});
 
 app.listen(3000, () => console.log('Servidor escuchando en puerto 3000'));
