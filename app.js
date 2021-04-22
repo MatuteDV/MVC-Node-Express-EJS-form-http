9c2be6015db28f0b105e01fdced43de51b3e5d41
Matias Vieira <matiasvieira0@gmail.com>
2021-04-22 14:42:48 -0300

    Se agrega respuesta a las solicitudes status 404

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ const methodOverride = require('method-override')
 // Requerimos este módulo para asegurar compatibilidad de métodos PUT y DELETE en todos los navegadores.
 app.use(methodOverride('_method'))
 // Para configurarlo indicamos a app que use este método, con app.use lo empleamos a nivel aplicación.
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use((req, res, next) => {
+    res.status(404).render("not-found");
+});
 
 // Rutas
-app.set('view engine', 'ejs');
 
 app.use('/', indexRoutes);
 app.use('/products', productsRoutes);
